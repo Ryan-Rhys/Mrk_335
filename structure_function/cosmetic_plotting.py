8cde06498220f09fe044696920fbfe2f1297fc00
Ryan-Rhys <ryangriff123@gmail.com>
2021-03-10 20:18:23 +0000

    Added code for generating gif images of boostrap uncertainties.

--- structure_function/cosmetic_plotting.py
+++ structure_function/cosmetic_plotting.py
@@ -37,9 +37,11 @@ if __name__ == '__main__':
 
     # Fit the GP UVW2 structure functions using a power law or broken power law
     power_law_model = PowerLaw1D()
-    broken_power_law_model = BrokenPowerLaw1D(alpha_1=1, alpha_2=1, x_break=100)
+    if kernel == 'RQ':
+        broken_power_law_model = BrokenPowerLaw1D(alpha_1=0, alpha_2=0, x_break=110)
+    else:
+        broken_power_law_model = BrokenPowerLaw1D(alpha_1=-1, alpha_2=0, x_break=110)
     fitter_plm = fitting.SimplexLSQFitter()
-    fitter_bplm = fitting.SimplexLSQFitter()
     power_law_tao_vals = uv_gp_tao_plot
     uv_gp_mean_structure_function *= 1e29  # multiply by a large number to prevent error in fitting power laws.
     uv_gp_std_structure_function *= 1e29
@@ -49,18 +51,21 @@ if __name__ == '__main__':
     # Use uncertainties to improve the fit as per this tutorial:
     # https://docs.astropy.org/en/stable/modeling/example-fitting-line.html#fit-using-uncertainties
 
-    plm = fitter_plm(power_law_model, power_law_tao_vals, power_law_sf_vals, weights=1/uv_gp_std_structure_function,
+    plm = fitter_plm(broken_power_law_model, power_law_tao_vals, power_law_sf_vals, weights=1/uv_gp_std_structure_function,
                       maxiter=5000)
 
-    plm_index1 = plm.alpha.value
+    plm_index1 = plm.alpha_1.value
+    plm_index2 = plm.alpha_2.value
+    x_break = plm.x_break.value
     print(plm_index1)
+    print(plm_index2)
+    print(x_break)
 
     # plot on the same axis
 
     fig, ax = plt.subplots(1)
     color = 'tab:blue'
     plt.xscale('log')
-    #ax.scatter(uv_tao_plot, uv_mean_structure_function, s=10, marker='+', color=color, label='obs')
     ax.errorbar(uv_tao_plot, uv_mean_structure_function, yerr=uv_std_structure_function, fmt='o', color=color,
                 markersize=3, linewidth=0.5, label='Observational')
     ax.set_yscale('log')
@@ -74,17 +79,15 @@ if __name__ == '__main__':
     ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
     ax2.set_ylabel('Gaussian Process SF', color=color, fontsize=12)  # we already handled the x-label with ax1
     ax2.plot(power_law_tao_vals, plm(power_law_tao_vals), label='Power Law', color='k')
-    #ax2.scatter(uv_gp_tao_plot, uv_gp_mean_structure_function, s=10, marker='x', color=color, label='gp')
     ax2.errorbar(uv_gp_tao_plot, uv_gp_mean_structure_function, yerr=uv_gp_std_structure_function.flatten(),
                  fmt='x', color=color, markersize=3, linewidth=0.5, label='GP')
     ax2.set_yscale('log')
     ax2.tick_params(axis='y', labelcolor=color)
-    #ax2.minorticks_off()
     ax2.yaxis.set_minor_locator(mticker.NullLocator())
     plt.tight_layout()
     fig.legend(loc=4, bbox_to_anchor=[0.25, 0.25, 0.6, 0])
     plt.savefig(f'cosmetic_figures/gp_structure_function_uv_{tag}_on_same_axis_'
-                f'alpha_is_{str(100*plm_index1)}.png')
+                f'alpha_is_{str(100*plm_index1)}_alpha2_is_{str(100*plm_index2)}_x_break_is_{str(100*x_break)}.png')
     plt.close()
 
     x_ray_gp_tao_plot = np.loadtxt(f'sf_gp_data/xray/{kernel}_{n_samples}_times')[2:]
